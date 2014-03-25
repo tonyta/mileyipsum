b3d46bf01d4df436c2915ddcc4fb6a2c78cf45e0
Tony Ta <tonyta.tt@gmail.com>
2014-03-24 22:05:46 -0500

    correct table seeding

--- miley_ipsum/db/seeds.rb
+++ miley_ipsum/db/seeds.rb
@@ -8,7 +8,12 @@ file.each do |line|
   prev_word = nil
   line.chomp.split(' ').each do |word_str|
     word = Word.find_or_create_by(word: word_str)
-    prev_word.children << word if prev_word
+
+    if prev_word && relationship = Relationship.find_by(child_id: word.id, parent_id: prev_word.id)
+      relationship.update(count: relationship.count + 1)
+    else
+      prev_word.children << word if prev_word
+    end
     prev_word = word
   end
 end
