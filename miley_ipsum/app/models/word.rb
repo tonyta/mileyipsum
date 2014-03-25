5daaa3bb113c9cab298a9bb62191ee271faaa347
Tony Ta <tonyta.tt@gmail.com>
2014-03-24 22:35:54 -0500

    implements next_word method for Word

--- miley_ipsum/app/models/word.rb
+++ miley_ipsum/app/models/word.rb
@@ -3,4 +3,13 @@ class Word < ActiveRecord::Base
   has_many :as_child_relationships, class_name: 'Relationship', foreign_key: :child_id
   has_many :children, through: :as_parent_relationships
   has_many :parents, through: :as_child_relationships
+
+  def next_word
+    relationships = self.as_parent_relationships
+    ids_probability_arr = []
+    relationships.each do |r|
+      r.count.times { ids_probability_arr << r.child_id }
+    end
+    Word.find(ids_probability_arr.sample)
+  end
 end
